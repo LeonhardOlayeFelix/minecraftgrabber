285266506898fa663d4f72ee86c4f83e2d9f57ab
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-22 01:57:13 +0100

    finished design of how recipe looks

--- src/components/CraftingTableGridElementComponent.tsx
+++ src/components/CraftingTableGridElementComponent.tsx
@@ -1,15 +1,26 @@
-import React from "react";
 import "./CraftingTableGridElementComponent.css";
 import { ItemsProps } from "../hooks/useMinecraftHook";
 
 interface Props {
   item: ItemsProps;
+  onClick?: () => void;
 }
 
-const CraftingTableGridElementComponent = ({ item }: Props) => {
+const CraftingTableGridElementComponent = ({ item, onClick }: Props) => {
   return (
-    <div className="grid-element">
-      {item && <img className="displayed" src={item.image} />}
+    <div className="grid-element image-container">
+      {item && (
+        <img
+          className="displayed"
+          src={item.image}
+          alt={item.name || "Image"}
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title={item.name || "click for more!"}
+          data-bs-delay={100}
+          style={{ cursor: "pointer" }}
+        />
+      )}
     </div>
   );
 };
