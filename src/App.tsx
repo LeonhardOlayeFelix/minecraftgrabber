a094ad2770331a0dfbb5fbfc3587eff1fb044f84
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-22 19:07:26 +0100

    Made Recipe Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import useBlocksAndItems, { RecipeProps } from "./hooks/useMinecraftHook";
 import CraftingTableComponent from "./components/CraftingTableComponent";
 import ItemSearchComponent from "./components/ItemSearchComponent";
+import RecipeComponent from "./components/RecipeComponent";
 
 //This is enough for the items.
 
@@ -18,10 +19,15 @@ const App = () => {
   return (
     <div className="center">
       <ItemSearchComponent items={items} className="m-3" maxResults={30} />
-      <CraftingTableComponent
+      {/* <CraftingTableComponent
         recipe={recipes.find((item) => item.item === "Beacon") as RecipeProps}
         items={items}
         className="m-3"
+      /> */}
+      <RecipeComponent
+        recipe={recipes.find((item) => item.item === "Beacon") as RecipeProps}
+        items={items}
+        onClick={() => 1}
       />
     </div>
   );
---
+++ src/components/RecipeComponent.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { ItemsProps, RecipeProps } from "../hooks/useMinecraftHook";
+import CraftingTableComponent from "./CraftingTableComponent";
+import "./RecipeComponent.css";
+interface Props {
+  recipe: RecipeProps;
+  items: ItemsProps[];
+  className?: string;
+  onClick: () => void;
+}
+
+const RecipeComponent = ({ recipe, items, onClick, className }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleOnClick = (event: HTMLImageElement) => {
+    console.log(event.alt);
+    onClick();
+  };
+  return (
+    <div id="screen" className="div-border">
+      <div>
+        {recipe && (
+          <div id="item-name-div">
+            <p id="item-name">Crafting</p>
+          </div>
+        )}
+        <div>
+          <CraftingTableComponent recipe={recipe} items={items} className="" />
+        </div>
+      </div>
+      <div className="arrow">&#10132;</div>
+      <div id="result">
+        {recipe && (
+          <img
+            className="displayed"
+            src={items.find((item) => item.name === recipe.item)?.image}
+            alt={recipe.item || "Image"}
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title={recipe.item || "click for more!"}
+            style={{ cursor: "pointer" }}
+            onClick={(event) => handleOnClick(event.target as HTMLImageElement)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RecipeComponent;
