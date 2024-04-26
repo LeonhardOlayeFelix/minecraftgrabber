0aceed58cd0716fe95d12a6e4de85dc31d643d59
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2024-04-26 21:26:00 +0100

    Made second version of arrows, which is definitely better than the first

--- src/components/CraftingRecipeComponent.tsx
+++ src/components/CraftingRecipeComponent.tsx
@@ -23,93 +23,69 @@ const CraftingRecipeComponent = ({
 }: Props) => {
   const carouselId = useId();
   return (
-    <div
-      style={{
-        width: craftingTableCellWidthHeight
-          ? `calc(${craftingTableCellWidthHeight} * 6.7)`
-          : `calc(var(--crafting-table-cell-width-height) * 6.1)`,
-      }}
-      id="carousel-container"
-      className={className}
-    >
-      <Flex id="arrows-flex-container">
-        <Box>
-          {recipes.length > 1 && (
-            <button
-              type="button"
-              data-bs-target={`#${carouselId}`}
-              data-bs-slide="prev"
-            >
-              <span aria-hidden="true" className="carousel-left-arrow">
-                &lt;
-              </span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          )}
-        </Box>
-        <Box>
-          {recipes.length > 1 && (
-            <button
-              type="button"
-              data-bs-target={`#${carouselId}`}
-              data-bs-slide="next"
-            >
-              <span aria-hidden="true" className="carousel-right-arrow">
-                &gt;
-              </span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          )}
-        </Box>
-      </Flex>
-      <div id="crafting-outer-div">
-        <div
-          id={carouselId}
-          className={"carousel carousel-dark slide"}
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-inner">
-            {recipes.map((recipe, index) => (
-              <div
-                key={index}
-                className={`carousel-item ${index === 0 ? "active" : ""}`}
-                data-bs-interval="4000"
-              >
+    <Flex flexDirection={"row"} alignItems={"center"}>
+      <Box>
+        {recipes.length > 1 && (
+          <button
+            type="button"
+            data-bs-target={`#${carouselId}`}
+            data-bs-slide="prev"
+          >
+            <span aria-hidden="true" className="carousel-left-arrow">
+              &lt;
+            </span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        )}
+      </Box>
+      <div
+        style={{
+          width: craftingTableCellWidthHeight
+            ? `calc(${craftingTableCellWidthHeight} * 6.7)`
+            : `calc(var(--crafting-table-cell-width-height) * 6.1)`,
+        }}
+        id="carousel-container"
+        className={className}
+      >
+        <div id="crafting-outer-div">
+          <div
+            id={carouselId}
+            className={"carousel carousel-dark slide"}
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              {recipes.map((recipe, index) => (
+                <div
+                  key={index}
+                  className={`carousel-item ${index === 0 ? "active" : ""}`}
+                  data-bs-interval="4000"
+                >
+                  <RecipeComponent
+                    recipe={recipe}
+                    gridElementAnimation={gridElementAnimation}
+                    gridResultAnimation={gridResultAnimation}
+                    items={items}
+                    craftingTableCellWidthHeight={craftingTableCellWidthHeight}
+                  />
+                </div>
+              ))}
+              {recipes.length === 0 && (
                 <RecipeComponent
-                  recipe={recipe}
+                  recipe={undefined as unknown as RecipeProps}
                   gridElementAnimation={gridElementAnimation}
                   gridResultAnimation={gridResultAnimation}
                   items={items}
                   craftingTableCellWidthHeight={craftingTableCellWidthHeight}
                 />
-              </div>
-            ))}
-            {recipes.length === 0 && (
-              <RecipeComponent
-                recipe={undefined as unknown as RecipeProps}
-                gridElementAnimation={gridElementAnimation}
-                gridResultAnimation={gridResultAnimation}
-                items={items}
-                craftingTableCellWidthHeight={craftingTableCellWidthHeight}
-              />
-            )}
+              )}
+            </div>
           </div>
-          {/* {recipes.length > 1 && (
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target={`#${carouselId}`}
-              data-bs-slide="prev"
-            >
-              <span className="carousel-left-arrow" aria-hidden="true">
-                &lt;
-              </span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-          )}
+        </div>
+      </div>
+      <Box>
+        <Box>
           {recipes.length > 1 && (
             <button
-              className="carousel-control-next"
               type="button"
               data-bs-target={`#${carouselId}`}
               data-bs-slide="next"
@@ -119,10 +95,10 @@ const CraftingRecipeComponent = ({
               </span>
               <span className="visually-hidden">Next</span>
             </button>
-          )} */}
-        </div>
-      </div>
-    </div>
+          )}
+        </Box>
+      </Box>
+    </Flex>
   );
 };
 
